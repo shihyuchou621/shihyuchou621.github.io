20767fa12f64fde0f1cfd965c3325ec100850af0
shihyuchou <kellyu621@gmail.com>
2020-10-30 15:16:37 +0800

    HW27

---
+++ precache-manifest.638f718e2b183d473091fe72549eca98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edf4e0f2578c3ca9904a792f31ab8b33",
+    "url": "/index.html"
+  },
+  {
+    "revision": "caa7a49b48ba59471d05",
+    "url": "/static/css/main.d02e35d1.chunk.css"
+  },
+  {
+    "revision": "05c18d4e1ab4d9d1f814",
+    "url": "/static/js/2.e4ddc7db.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.e4ddc7db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "caa7a49b48ba59471d05",
+    "url": "/static/js/main.89354f4e.chunk.js"
+  },
+  {
+    "revision": "de9d7b2868e288e79b25",
+    "url": "/static/js/runtime-main.bb9ee77d.js"
+  }
+]);
